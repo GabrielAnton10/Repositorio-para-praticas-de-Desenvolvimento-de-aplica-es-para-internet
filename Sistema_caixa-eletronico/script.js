11e9e04549581cc81f0800b20367a91baf48b46e
Gabriel Antônio <144959941+GabrielAnton10@users.noreply.github.com>
2025-10-28 11:19:14 -0300

    inicio das fariaveis de javascript
    iniciei todas as variaveis agora é só criar as funsoes dos botão.

--- Sistema_caixa-eletronico/script.js
+++ Sistema_caixa-eletronico/script.js
@@ -1,2 +1,8 @@
 let Depositar = document.getElementsByClassName("Depositar")[0];
+let Sacar = document.getElementsByClassName("Sacar")[0];
+let Saldo = document.getElementsByClassName("Saldo")[0];
+let Valor = document.getElementsByClassName("Valor")[0];
+let Confirmar = document.getElementsByClassName("Confirmar")[0];
+
+
 
